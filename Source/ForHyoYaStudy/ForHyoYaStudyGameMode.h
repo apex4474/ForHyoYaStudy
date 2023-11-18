3a8d7f4c295a95d32dfa14be0dd30ad0e133dad5
Undev <apex4474@gmail.com>
2023-11-18 23:13:59 +0900

    FirstUpload

---
+++ Source/ForHyoYaStudy/ForHyoYaStudyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ForHyoYaStudyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AForHyoYaStudyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AForHyoYaStudyGameMode();
+};
+
+
+
